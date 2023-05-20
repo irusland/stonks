b05340467f8bbdac81ae87c27bdd049273d1d83b
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2023-05-20 20:26:27 +0300

    regression stock env

--- notebooks/gym.py
+++ notebooks/gym.py
@@ -1,114 +1,92 @@
-import gymnasium
 import gymnasium as gym
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import pygame
+from IPython import display
 from gymnasium import spaces
-from gymnasium.envs.registration import register
-from gymnasium.spaces import Box
-from gymnasium.spaces import Discrete
+from sklearn.preprocessing import MinMaxScaler
 
 
-class RelativePosition(gymnasium.ObservationWrapper):
-    def __init__(self, env):
-        super().__init__(env)
-        self.observation_space = Box(shape=(2,), low=-np.inf, high=np.inf)
+class RegressionStocksEnv(gym.Env):
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
-    def observation(self, obs):
-        return obs["target"] - obs["agent"]
-
-
-class ReacherRewardWrapper(gymnasium.Wrapper):
-    def __init__(self, env, reward_dist_weight, reward_ctrl_weight):
-        super().__init__(env)
-        self.reward_dist_weight = reward_dist_weight
-        self.reward_ctrl_weight = reward_ctrl_weight
-
-    def step(self, action):
-        obs, _, terminated, truncated, info = self.env.step(action)
-        reward = (
-                self.reward_dist_weight * info["reward_dist"]
-                + self.reward_ctrl_weight * info["reward_ctrl"]
-        )
-        return obs, reward, terminated, truncated, info
-
-
-class DiscreteActions(gymnasium.ActionWrapper):
-    def __init__(self, env, disc_to_cont):
-        super().__init__(env)
-        self.disc_to_cont = disc_to_cont
-        self.action_space = Discrete(len(disc_to_cont))
-
-    def action(self, act):
-        return self.disc_to_cont[act]
-
-
-class GridWorldEnv(gymnasium.Env):
-    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 3}
-
-    def __init__(self, render_mode=None, size=5):
+    def __init__(self, df, render_mode=None, size=5, fps=4):
         self.size = size  # The size of the square grid
         self.window_size = 512  # The size of the PyGame window
 
-        # Observations are dictionaries with the agent's and the target's location.
-        # Each location is encoded as an element of {0, ..., `size`}^2, i.e. MultiDiscrete([size, size]).
-        self.observation_space = spaces.Dict(
-            {
-                "agent": spaces.Box(0, size - 1, shape=(2,), dtype=int),
-                "target": spaces.Box(0, size - 1, shape=(2,), dtype=int),
-            }
+        self._original_df = df
+
+        self._scaler = MinMaxScaler()
+
+        df = pd.DataFrame(self._scaler.fit_transform(df), columns=df.columns)
+
+        #         df = pd.DataFrame(scaler.inverse_transform(target_df), columns=df.columns)
+
+        X = df.iloc[:, :-1]
+        self._X = X
+        y = df.iloc[:, -1]
+        self._y = y
+
+        self._taget_column_name = self._y.name
+
+        self._action_indent = 1e-3
+        action_min = y.min() - self._action_indent
+        self._action_min = action_min
+        action_max = y.max() + self._action_indent
+        self._action_max = action_max
+
+        self._initial_taget_location = y[0]
+
+        feature_number = X.shape[1]
+
+        self._dataset = StockDataset(X, y)
+
+        # observing current state of market and news
+        describe = X.describe()
+        self.observation_space = spaces.Box(
+            low=describe.loc['min'].to_numpy(),
+            high=describe.loc['max'].to_numpy(),
+            shape=(feature_number,), dtype=np.float32
         )
 
-        # We have 4 actions, corresponding to "right", "up", "left", "down", "right"
-        self.action_space = spaces.Discrete(2)
-
-        """
-        The following dictionary maps abstract actions from `self.action_space` to 
-        the direction we will walk in if that action is taken.
-        I.e. 0 corresponds to "right", 1 to "up" etc.
-        """
-        self._action_to_direction = {
-            0: np.array([1, 0]),
-            1: np.array([0, 1]),
-            2: np.array([-1, 0]),
-            3: np.array([0, -1]),
-        }
+        # prediction of to buy or to sell relatively
+        #         self.action_space = spaces.Box(
+        #             low=-max_action_bound,
+        #             high=+max_action_bound,
+        #             dtype=np.float32,
+        #         )
+        # prediction of close price
+        self.action_space = spaces.Box(
+            low=action_min,
+            high=action_max,
+            dtype=np.float32,
+        )
 
         assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
 
-        """
-        If human-rendering is used, `self.window` will be a reference
-        to the window that we draw to. `self.clock` will be a clock that is used
-        to ensure that the environment is rendered at the correct framerate in
-        human-mode. They will remain `None` until human-mode is used for the
-        first time.
-        """
         self.window = None
         self.clock = None
 
     def _get_obs(self):
-        return {"agent": self._agent_location, "target": self._target_location}
+        features, target = self._dataset[self._current_dataset_idx]
+        return features
 
     def _get_info(self):
-        return {
-            "distance": np.linalg.norm(
-                self._agent_location - self._target_location, ord=1
-            )
-        }
+        return self._current_dataset_idx
 
     def reset(self, seed=None, options=None):
-        # We need the following line to seed self.np_random
         super().reset(seed=seed)
 
-        # Choose the agent's location uniformly at random
-        self._agent_location = self.np_random.integers(0, self.size, size=2, dtype=int)
+        self._agent_history = []
 
-        # We will sample the target's location randomly until it does not coincide with the agent's location
-        self._target_location = self._agent_location
-        while np.array_equal(self._target_location, self._agent_location):
-            self._target_location = self.np_random.integers(
-                0, self.size, size=2, dtype=int
-            )
+        #         self._agent_location = self.np_random.integers(self._obs_min, self._obs_max, size=1, dtype=np.float32)
+        self._agent_location = self._initial_taget_location
+
+        self._target_location = self._initial_taget_location
+
+        self._current_dataset_idx = 0
 
         observation = self._get_obs()
         info = self._get_info()
@@ -119,123 +97,50 @@ class GridWorldEnv(gymnasium.Env):
         return observation, info
 
     def step(self, action):
-        # Map the action (element of {0,1,2,3}) to the direction we walk in
-        direction = self._action_to_direction[action]
-        # We use `np.clip` to make sure we don't leave the grid
-        self._agent_location = np.clip(
-            self._agent_location + direction, 0, self.size - 1
-        )
-        # An episode is done iff the agent has reached the target
-        terminated = np.array_equal(self._agent_location, self._target_location)
-        reward = 1 if terminated else 0  # Binary sparse rewards
+        self._agent_history.append(action)
+
+        terminated = self._current_dataset_idx >= len(self._dataset) - 1
+
+        target = self._y[self._current_dataset_idx]
+
+        reward = 1 / np.abs(action - target)
+
         observation = self._get_obs()
         info = self._get_info()
 
         if self.render_mode == "human":
             self._render_frame()
 
+        self._current_dataset_idx += 1
         return observation, reward, terminated, False, info
 
     def render(self):
         if self.render_mode == "rgb_array":
             return self._render_frame()
 
+    def _inv_transform(self, data):
+        scaler = self._scaler
+        dummy = pd.DataFrame(np.zeros((len(data), len(self._original_df.columns))), columns=self._original_df.columns)
+        dummy[self._taget_column_name] = data
+        return scaler.inverse_transform(dummy)
+
     def _render_frame(self):
-        if self.window is None and self.render_mode == "human":
-            pygame.init()
-            pygame.display.init()
-            self.window = pygame.display.set_mode((self.window_size, self.window_size))
-        if self.clock is None and self.render_mode == "human":
-            self.clock = pygame.time.Clock()
+        target = self._y[:self._current_dataset_idx]
+        agent = self._agent_history
 
-        canvas = pygame.Surface((self.window_size, self.window_size))
-        canvas.fill((255, 255, 255))
-        pix_square_size = (
-                self.window_size / self.size
-        )  # The size of a single grid square in pixels
+        plt.figure(3)
+        plt.clf()
+        plt.cla()
+        plt.plot(target, label='target')
+        plt.plot(agent, label='agent')
 
-        # First we draw the target
-        pygame.draw.rect(
-            canvas,
-            (255, 0, 0),
-            pygame.Rect(
-                pix_square_size * self._target_location,
-                (pix_square_size, pix_square_size),
-            ),
-        )
-        # Now we draw the agent
-        pygame.draw.circle(
-            canvas,
-            (0, 0, 255),
-            (self._agent_location + 0.5) * pix_square_size,
-            pix_square_size / 3,
-        )
-
-        # Finally, add some gridlines
-        for x in range(self.size + 1):
-            pygame.draw.line(
-                canvas,
-                0,
-                (0, pix_square_size * x),
-                (self.window_size, pix_square_size * x),
-                width=3,
-            )
-            pygame.draw.line(
-                canvas,
-                0,
-                (pix_square_size * x, 0),
-                (pix_square_size * x, self.window_size),
-                width=3,
-            )
-
-        if self.render_mode == "human":
-            # The following line copies our drawings from `canvas` to the visible window
-            self.window.blit(canvas, canvas.get_rect())
-            pygame.event.pump()
-            pygame.display.update()
-
-            # We need to ensure that human-rendering occurs at the predefined framerate.
-            # The following line will automatically add a delay to keep the framerate stable.
-            self.clock.tick(self.metadata["render_fps"])
-        else:  # rgb_array
-            return np.transpose(
-                np.array(pygame.surfarray.pixels3d(canvas)), axes=(1, 0, 2)
-            )
+        plt.title("%s | Step: %d" % (env, self._current_dataset_idx))
+        plt.ylim((target.min() - 0, target.max() + 0))
+        plt.legend()
+        display.clear_output(wait=True)
+        display.display(plt.gcf())
 
     def close(self):
         if self.window is not None:
             pygame.display.quit()
             pygame.quit()
-
-
-class ClipReward(gymnasium.RewardWrapper):
-    def __init__(self, env, min_reward, max_reward):
-        super().__init__(env)
-        self.min_reward = min_reward
-        self.max_reward = max_reward
-        self.reward_range = (min_reward, max_reward)
-
-    def reward(self, reward):
-        return np.clip(reward, self.min_reward, self.max_reward)
-
-
-register(
-    id="gym/GridWorld-v0",
-    entry_point="notebooks.gym:GridWorldEnv",
-    max_episode_steps=300,
-)
-
-# env = gym.make('gym/GridWorld-v0', render_mode="rgb_array")
-env = gym.make('gym/GridWorld-v0', render_mode="human")
-
-print(env.action_space)
-
-# env = gym.make("LunarLander-v2", render_mode="human")
-observation, info = env.reset(seed=42)
-for _ in range(1000):
-    action = env.action_space.sample()  # this is where you would insert your policy
-    observation, reward, terminated, truncated, info = env.step(action)
-
-    if terminated or truncated:
-        observation, info = env.reset()
-env.close()
